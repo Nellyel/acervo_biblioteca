9d3735f4ce722a2f7c048414b18cf6f884766bc2
Nellyel <endrielmedeiros@gmail.com>
2026-09-23 20:44:55 -0300

    Implementa validacao do ano de publicacao

--- acervo/forms.py
+++ acervo/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from .models import Livro
+from datetime import date
 
 class LivroForm(forms.ModelForm):
     class Meta:
+        
         model = Livro
         fields = [
             'titulo',
@@ -11,3 +13,13 @@ class LivroForm(forms.ModelForm):
             'tipo_acervo',
             'categoria',
         ]
+        
+    def clean_ano(self):
+        ano = self.cleaned_data.get('ano')
+    
+        if ano and ano > date.today().year:
+            raise forms.ValidationError(
+                'O ano de publicação não pode estar no futuro.'
+            )
+            
+        return ano
